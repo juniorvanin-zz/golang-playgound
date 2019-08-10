c5ac219b9371ea8a16fe368c64f8b207c4f1399b
Gilmar Vanin <jvanin@thoughtworks.com>
2019-08-10 15:43:59 -0300

    Adds index and value of each arg

--- helloworld.go
+++ helloworld.go
@@ -3,8 +3,17 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
 )
 
 func main() {
-	fmt.Println(os.Args[1:])
+	var result string
+	var whitespaceChar = " "
+
+	for index, arg := range os.Args {
+		result += strconv.Itoa(index) + whitespaceChar + arg
+		result += whitespaceChar
+	}
+
+	fmt.Println(result)
 }
