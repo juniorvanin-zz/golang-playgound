d00b5f32535d475b4aa12fd75ea71451f05a0b62
Gilmar Vanin <jvanin@thoughtworks.com>
2019-08-18 14:19:53 -0300

    Use flag package to better understand pointers

---
+++ echo4.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"strings"
+)
+
+var n = flag.Bool("n", false, "omit trailling newline")
+var sep = flag.String("s", " ", "separator")
+func main() {
+	flag.Parse()
+	fmt.Println(strings.Join(flag.Args(), *sep))
+	if !*n {
+		fmt.Println()
+	}
+}
